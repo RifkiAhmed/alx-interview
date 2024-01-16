927a6afd28103c583d63fc235c82b7c606a26a6f
ahmed <rifkiahmed.91@gmail.com>
2024-01-16 11:14:41 +0100

    Fix checks

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -3,7 +3,6 @@
 Minimum number of copy all and paste operations required to result in
 exactly n H characters in the file
 '''
-# from typing import Tuple
 
 
 def copy_all(i, text):
@@ -22,38 +21,28 @@ def paste(i, text, copy):
 
 def minOperations(n):
     '''
-    Return the minimum number of copy all and paste operations to create a text
-    file of n 'H' characters.
+    Return the minimum number of copy all and paste operations to create a
+    text file of n 'H' characters.
 
     Args:
     n (int): The number of 'H' characters to create in the text.
 
     Returns:
-    int: The minimum number of copy all and paste operations required to create
-    a text file with n 'H' characters. if n is impossible to achieve 0 isreturn
+    int: The minimum number of copy all and paste operations required to
+    create a text file with n 'H' characters. if n is impossible to
+    achieve 0 is returned.
     '''
-    text: str = 'H'
-    copy: str = ''
-    i: int = 0
+    text = 'H'
+    copy = ''
+    i = 0
 
     if not isinstance(n, int):
         return 0
 
-    while len(text) <= n:
-        if len(text) == n:
-            return i
-
-        if len(text) == 1:
-            # print('H', end='')
-            i, copy = copy_all(i, text)
-            i, text = paste(i, text, copy)
-            if len(text) == n:
-                # print('\n')
-                return i
-
+    while (len(text) < n):
         if n % len(text) == 0:
             i, copy = copy_all(i, text)
-            while True:
+            while (True):
                 i, text = paste(i, text, copy)
                 if len(text) == n:
                     # print('\n')
@@ -65,29 +54,29 @@ def minOperations(n):
             if len(text) == n:
                 # print('\n')
                 return i
-    return 0
+    return i if len(text) == n else 0
 
 
-# n = 0
-# print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-# # -> Min # of operations to reach 0 char: 0
+n = 0
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+# -> Min # of operations to reach 0 char: 0
 
-# n = 1
-# print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-# # -> Min # of operations to reach 1 char: 0
+n = 1
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+# -> Min # of operations to reach 1 char: 0
 
-# n = 2
-# print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-# # -> Min # of operations to reach 2 char: 2
+n = 2
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+# -> Min # of operations to reach 2 char: 2
 
-# n = 4
-# print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-# # -> Min # of operations to reach 4 char: 4
+n = 4
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+# -> Min # of operations to reach 4 char: 4
 
-# n = 9
-# print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-# # -> Min # of operations to reach 9 char: 6
+n = 9
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+# -> Min # of operations to reach 9 char: 6
 
-# n = 12
-# print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-# # -> Min # of operations to reach 12 char: 7
+n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+# -> Min # of operations to reach 12 char: 7
