10b3c668904e4c4df28a30177ed18b11099932de
ahmed <rifkiahmed.91@gmail.com>
2024-01-25 22:35:33 +0100

    Test a new implementation

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -8,10 +8,10 @@ count = 0
 size = 0
 status = {"200": 0, "301": 0, "400": 0, "401": 0,
           "403": 0, "404": 0, "405": 0, "500": 0}
-# pattern = re.compile(
-#     r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
-#     r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] '
-#     r'"GET \/projects\/260 HTTP\/1\.1" (.*) (\d+)')
+pattern = re.compile(
+    r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+    r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] '
+    r'"GET \/projects\/260 HTTP\/1\.1" (.*) (\d+)')
 
 
 def print_statistics():
@@ -26,13 +26,11 @@ try:
     while True:
         line = sys.stdin.readline()
         count += 1
-        line_parts = line.split()
-        try:
-            size += int(line_parts[-1])
-            if line_parts[-2] in status:
-                status[line_parts[-2]] += 1
-        except (IndexError, ValueError):
-            pass
+        if re.match(pattern, line):
+            line_list = line.split()
+            size += int(line_list[-1])
+            if line_list[-2] in status:
+                status[line_list[-2]] += 1
         if not line:
             print_statistics()
             break
