365e4a83c08645127f0ec893dadddb7e8342d2f2
ahmed <rifkiahmed.91@gmail.com>
2024-02-01 22:10:58 +0100

    Add test cases

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -26,3 +26,12 @@ def validUTF8(data):
             return False
 
     return True
+
+# data = [65]
+# print(validUTF8(data)) # -> True
+
+# data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+# print(validUTF8(data)) # -> True
+
+# data = [229, 65, 127, 256]
+# print(validUTF8(data)) # -> False
