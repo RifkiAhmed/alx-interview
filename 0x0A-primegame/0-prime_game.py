e4f8868ea8d913327aa50b61256bd302e5cea56e
ahmed <rifkiahmed.91@gmail.com>
2024-03-12 22:45:17 +0000

    Determine the winner of prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -27,7 +27,7 @@ def count_primes(n):
 def isWinner(x, nums):
     """ Returns prime game winner, else returns None
     """
-    if x <= 0 or not nums:
+    if x <= 0 or not nums or x != len(nums):
         return None
 
     maria_wins = 0
