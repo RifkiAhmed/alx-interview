24314d04ac6c7ba72684715667e967738092ca87
ahmed <rifkiahmed.91@gmail.com>
2024-03-13 21:32:14 +0000

    Determine the winner of prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -3,25 +3,22 @@
 """
 
 
-def is_prime(n):
-    """ Checks if n is a prime number
+def prime_sieve(n):
+    """ Returns a list of prime numbers up to n
     """
-    if n < 2:
-        return False
+    sieve = [True] * (n + 1)
+    sieve[0] = sieve[1] = False
     for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+        if sieve[i]:
+            for j in range(i * i, n + 1, i):
+                sieve[j] = False
+    return sieve
 
 
-def count_primes(n):
-    """ Counts the number of prime numbers up to n inclusive
+def count_primes_up_to_n(n, primes):
+    """ Counts the number of prime numbers up to n
     """
-    count = 0
-    for i in range(2, n + 1):
-        if is_prime(i):
-            count += 1
-    return count
+    return sum(primes[:n + 1])
 
 
 def isWinner(x, nums):
@@ -30,15 +27,13 @@ def isWinner(x, nums):
     if x <= 0 or not nums or x != len(nums):
         return None
 
-    maria_wins = 0
-    ben_wins = 0
-
-    for n in nums:
-        total_primes = count_primes(n)
-        if total_primes % 2 == 0:
-            ben_wins += 1
-        else:
-            maria_wins += 1
+    max_num = max(nums)
+    primes = prime_sieve(max_num)
+    maria_wins = sum(
+        1 for n in nums if count_primes_up_to_n(
+            n, primes) %
+        2 != 0)
+    ben_wins = x - maria_wins
 
     if maria_wins == ben_wins:
         return None
